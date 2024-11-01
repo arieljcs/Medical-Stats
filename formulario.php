a272ea69f27bd7b508b1074771a7d02a03c9a7ae
yramirez10 <yeison.araujo.1995@hotmail.com>
2024-11-01 00:01:30 -0300

    Update formulario.php

--- formulario.php
+++ formulario.php
@@ -6,19 +6,24 @@ if (!isset($_SESSION['usuario'])) {
     header('Location: login.php');
     exit();
 }
+//Si se presionó un boton para modificar un registro
 if (isset($_POST['boton'])) {
+    //Obtner el boton que se presionó
     $botonPresionado = $_POST['boton'];
     if ($botonPresionado != Null) {
-
+        //Iniciar conexión
         $conexion = mysqli_connect("localhost", "root", "", "medical_stats") or
             die("Problemas con la conexión");
-
+        //Obtener ID del boton
         $id = substr($botonPresionado, 5);
-
+        //Obtener datos de la tabla de pacientes de acuerdo al ID que se quiere modificar
         $registros = mysqli_query($conexion, "select * from pacientes where id = $id;") or
             die("Problemas en el select:" . mysqli_error($conexion));
 
         while ($reg = mysqli_fetch_array($registros)) {
+            //---------------------------------------------------------------------
+            //Armar el formulario con la información del ID que se quiere modificar
+            //---------------------------------------------------------------------
 ?>
 
             <!DOCTYPE html>
@@ -38,7 +43,7 @@ if (isset($_POST['boton'])) {
                         <li><a href="stock.php">Control de Stock</a></li>
                         <li> <a href="pagina.php"><img src="LogoB.png" alt="Logo" class="logo"></a></li>
                         <li><a href="estadistica.php">Estadística</a></li>
-                        <li><a href="salir.php" ><img src='CerrarSesion.png' alt='CerrarSesion' width='40' height='40'></a></li>
+                        <li><a href="salir.php"><img src='CerrarSesion.png' alt='CerrarSesion' width='40' height='40'></a></li>
                     </ul>
                 </nav>
 
@@ -48,12 +53,6 @@ if (isset($_POST['boton'])) {
                         <form action="insertar.php" method="post">
 
                             <input type="hidden" id="id" name="id" <?php echo "value='" . $reg['id'] . "'"; ?>>
-
-                          <!-- <label for="fecha">Fecha:</label>
-                <input type="date" id="text" name="fecha"   required>
--->
-               
-    
                             <label for="fecha">Fecha:</label>
                             <input type="time" id="fecha" name="fecha" <?php echo "value='" . $reg['fecha'] . "'"; ?> required>
                             <label for="quirofano">Número quirófano:</label>
@@ -93,100 +92,104 @@ if (isset($_POST['boton'])) {
                             <input type="text" id="instrumentador" name="instrumentador" <?php echo "value='" . $reg['instrumentador'] . "'"; ?> required>
 
                             <p class="Urgencia">
-                            <input type="checkbox" id="urgencia" name="instrumentador" <?php echo "value='" . $reg['urgencia'] . "'"; ?> >Urgencia
+                                <input type="checkbox" id="urgencia" name="instrumentador" <?php echo "value='" . $reg['urgencia'] . "'"; ?>>Urgencia
                             </p>
 
                             <input type="submit" value="Guardar">
                         </form>
                     </div>
                 </main>
-    <?php
+        <?php
         }
     }
-}else{
-    ?>
+} else {
+    // Si NO se quiere modificar un registro, se quiere crear uno nuevo
+    //---------------------------------------------------------------------
+    //Armar el formulario HTML Vacío
+    //---------------------------------------------------------------------
+        ?>
 
-<!DOCTYPE html>
-            <html lang="es">
+        <!DOCTYPE html>
+        <html lang="es">
 
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Formulario de Registro</title>
-                <link rel="stylesheet" href="styles.css">
-            </head>
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Formulario de Registro</title>
+            <link rel="stylesheet" href="styles.css">
+        </head>
 
-            <body>
-        
-    <nav>
-        <ul>
-            <li><a href="pagina.php">Inicio</a></li>
-            <li><a href="stock.php">Control de Stock</a></li>
-            <li> <a href="pagina.php"><img src="LogoB.png" alt="Logo" class="logo"></a></li>
-            <li><a href="estadistica.php">Estadística</a></li>
-            <li><a href="salir.php">Cerrar Sesión</a></li>
-        </ul>
-    </nav>
+        <body>
 
-                <main>
-                    <h1>Formulario de Registro</h1>
-                    <div class="form-container">
-                        <form action="insertar.php" method="post">
+            <nav>
+                <ul>
+                    <li><a href="pagina.php">Inicio</a></li>
+                    <li><a href="stock.php">Control de Stock</a></li>
+                    <li> <a href="pagina.php"><img src="LogoB.png" alt="Logo" class="logo"></a></li>
+                    <li><a href="estadistica.php">Estadística</a></li>
+                    <li><a href="salir.php">Cerrar Sesión</a></li>
+                </ul>
+            </nav>
 
-                            <input type="hidden" id="id" name="id"  required>
+            <main>
+                <h1>Formulario de Registro</h1>
+                <div class="form-container">
+                    <form action="insertar.php" method="post">
 
-                             <label for="fecha">Fecha:</label>
-                            <input type="date" id="fecha" name="fecha" required>
+                        <input type="hidden" id="id" name="id" required>
 
-                            <label for="quirofano">Número quirófano:</label>
-                            <input type="text" id="quirofano" name="numero_quirofano" required>
+                        <label for="fecha">Fecha:</label>
+                        <input type="date" id="fecha" name="fecha" required>
 
-                            <label for="edad">Edad</label>
-                            <input type="text" id="edad" name="edad" required>
+                        <label for="quirofano">Número quirófano:</label>
+                        <input type="text" id="quirofano" name="numero_quirofano" required>
 
-                            <label for="dni">DNI</label>
-                            <input type="text" id="dni" name="dni" required>
+                        <label for="edad">Edad</label>
+                        <input type="text" id="edad" name="edad" required>
 
-                            <label for="Localidad">Localidad:</label>
-                            <input type="text" id="Localidad" name="Localidad" required>
+                        <label for="dni">DNI</label>
+                        <input type="text" id="dni" name="dni" required>
 
-                            <label for="apellido_nombre">Apellido y Nombre:</label>
-                            <input type="text" id="apellido_nombre" name="nombre"  required>
+                        <label for="Localidad">Localidad:</label>
+                        <input type="text" id="Localidad" name="Localidad" required>
 
-                            <label for="cirugia">Procedimiento:</label>
-                            <input type="text" id="cirugia" name="procedimiento"  required>
+                        <label for="apellido_nombre">Apellido y Nombre:</label>
+                        <input type="text" id="apellido_nombre" name="nombre" required>
 
-                            <label for="cirujano">Cirujano:</label>
-                            <input type="text" id="cirujano" name="cirujano"  required>
+                        <label for="cirugia">Procedimiento:</label>
+                        <input type="text" id="cirugia" name="procedimiento" required>
 
-                            <label for="ayudante1">1° Ayudante:</label>
-                            <input type="text" id="ayudante1" name="ayudante1"required>
+                        <label for="cirujano">Cirujano:</label>
+                        <input type="text" id="cirujano" name="cirujano" required>
 
-                            <label for="ayudante2">2° Ayudante:</label>
-                            <input type="text" id="ayudante2" name="ayudante2"  required>
+                        <label for="ayudante1">1° Ayudante:</label>
+                        <input type="text" id="ayudante1" name="ayudante1" required>
 
-                            <label for="anestesista">Anestesista:</label>
-                            <input type="text" id="anestesista" name="anestesista"  required>
+                        <label for="ayudante2">2° Ayudante:</label>
+                        <input type="text" id="ayudante2" name="ayudante2" required>
 
-                            <label for="tipo_anestesia">Tipo anestesia:</label>
-                            <input type="text" id="tipo_anestesia" name="tipo_anestesia"  required>
+                        <label for="anestesista">Anestesista:</label>
+                        <input type="text" id="anestesista" name="anestesista" required>
 
-                            <label for="instrumentador">Instrumentador:</label>
-                            <input type="text" id="instrumentador" name="instrumentador"  required>
+                        <label for="tipo_anestesia">Tipo anestesia:</label>
+                        <input type="text" id="tipo_anestesia" name="tipo_anestesia" required>
 
-                            <p class="Urgencia">
-                            <input type="checkbox" id="urgencia" name="instrumentador" >Urgencia
-                            </p>
-                            <input type="submit" value="Guardar">
-                        </form>
-                    </div>
-                </main>
-<?php
-}
-?>
-    <footer>
-        <p>®️2024 Medical Stats. Todos los derechos reservados.</p>
-    </footer>
-            </body>
+                        <label for="instrumentador">Instrumentador:</label>
+                        <input type="text" id="instrumentador" name="instrumentador" required>
 
-            </html>
+                        <p class="Urgencia">
+                            <input type="checkbox" id="urgencia" name="instrumentador">Urgencia
+                        </p>
+                        <input type="submit" value="Guardar">
+                    </form>
+                </div>
+            </main>
+        <?php
+    }
+        ?>
+        <footer>
+            <p>®️2024 Medical Stats. Todos los derechos reservados.</p>
+        </footer>
+        </body>
+
+        </html>
