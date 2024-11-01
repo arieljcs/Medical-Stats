03b378cc04470aa1ee93a9049e7ed3ea791f5ad2
yramirez10 <yeison.araujo.1995@hotmail.com>
2024-11-01 00:05:34 -0300

    Update pagina.php

--- pagina.php
+++ pagina.php
@@ -6,20 +6,22 @@ if (!isset($_SESSION['usuario'])) {
     header('Location: login.php');
     exit();
 }
-
+//Iniciar conexión
 $conexion = mysqli_connect("localhost", "root", "", "medical_stats") or die("Problemas con la conexión");
-
+//Obtener los ultimos 5 registros de pacientes ingresados 
 $registros = mysqli_query($conexion, "select * from pacientes order by id DESC LIMIT 5;") or die("Problemas en el select:" . mysqli_error($conexion));
 ?>
-
+<!-- Generar el HTML -->
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Medical Stats</title>
     <link rel="stylesheet" href="styles.css">
 </head>
+
 <body>
     <nav>
         <ul>
@@ -33,78 +35,82 @@ $registros = mysqli_query($conexion, "select * from pacientes order by id DESC L
     <main>
         <h1>Registro de Pacientes</h1>
         <div class="table-container">
-        <table id="tablaDatos">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Fecha</th>
-                    <th>Numero quirofano</th>
-                    <th>Edad</th>
-                    <th>DNI</th>
-                    <th>Localidad</th>
-                    <th>Apellido y Nombre</th>
-                    <th>Cirugia</th>
-                    <th>Cirujano</th>
-                    <th>1° Ayudante</th>
-                    <th>2° Ayudante</th>
-                    <th>Anestesista</th>
-                    <th>Instrumentador</th>
-                    <th>Tipo anestesia</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                <form action="formulario.php" method="POST">
-                    <?php
-                    while ($reg = mysqli_fetch_array($registros)) {
-                        $botton_name = "name_" . $reg['id'];
-                        echo "<tr>";
-                        echo "<td>" . $reg['id'] . "</td>";
-                        echo "<td>" . $reg['fecha'] . "</td>";
-                        echo "<td>" . $reg['numero_quirofano'] . "</td>";
-                        echo "<td>" . $reg['edad'] . "</td>";
-                        echo "<td>" . $reg['dni'] . "</td>";
-                        echo "<td>" . $reg['Localidad'] . "</td>";
-                        echo "<td>" . $reg['nombre'] . "</td>";
-                        echo "<td>" . $reg['procedimiento'] . "</td>";
-                        echo "<td>" . $reg['cirujano'] . "</td>";
-                        echo "<td>" . $reg['1_Ayudante'] . "</td>";
-                        echo "<td>" . $reg['2_Ayudante'] . "</td>";
-                        echo "<td>" . $reg['anestesista'] . "</td>";
-                        echo "<td>" . $reg['instrumentador'] . "</td>";
-                        echo "<td>" . $reg['tipo_anestesia'] . "</td>";
-                        echo "<td><button class='btn-secondary' name='boton' value='$botton_name'><img src='edit.png' alt='Editar' width='40' height='35'></button></td>";
-                        echo "</tr>";
-                    }
-                    ?>
-                </form>
-            </tbody>
-        </table>
+            <table id="tablaDatos">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Fecha</th>
+                        <th>Numero quirofano</th>
+                        <th>Edad</th>
+                        <th>DNI</th>
+                        <th>Localidad</th>
+                        <th>Apellido y Nombre</th>
+                        <th>Cirugia</th>
+                        <th>Cirujano</th>
+                        <th>1° Ayudante</th>
+                        <th>2° Ayudante</th>
+                        <th>Anestesista</th>
+                        <th>Instrumentador</th>
+                        <th>Tipo anestesia</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <form action="formulario.php" method="POST">
+                        <?php
+                        while ($reg = mysqli_fetch_array($registros)) {
+                            $botton_name = "name_" . $reg['id'];
+                            echo "<tr>";
+                            echo "<td>" . $reg['id'] . "</td>";
+                            echo "<td>" . $reg['fecha'] . "</td>";
+                            echo "<td>" . $reg['numero_quirofano'] . "</td>";
+                            echo "<td>" . $reg['edad'] . "</td>";
+                            echo "<td>" . $reg['dni'] . "</td>";
+                            echo "<td>" . $reg['Localidad'] . "</td>";
+                            echo "<td>" . $reg['nombre'] . "</td>";
+                            echo "<td>" . $reg['procedimiento'] . "</td>";
+                            echo "<td>" . $reg['cirujano'] . "</td>";
+                            echo "<td>" . $reg['1_Ayudante'] . "</td>";
+                            echo "<td>" . $reg['2_Ayudante'] . "</td>";
+                            echo "<td>" . $reg['anestesista'] . "</td>";
+                            echo "<td>" . $reg['instrumentador'] . "</td>";
+                            echo "<td>" . $reg['tipo_anestesia'] . "</td>";
+                            echo "<td><button class='btn-secondary' name='boton' value='$botton_name'><img src='edit.png' alt='Editar' width='40' height='35'></button></td>";
+                            echo "</tr>";
+                        }
+                        ?>
+                    </form>
+                </tbody>
+            </table>
         </div>
+        <!----------------------------------------------------------------------------------------------
+        Función para imprimir la tabla
+        ---------------------------------------------------------------------------------------------->
         <script>
-    function imprimirTabla() {
-        var divToPrint = document.getElementById("tablaDatos");
-        if (divToPrint) {
-            var newWin = window.open("");
-            newWin.document.write('<html><head><<title>Imprimir Tabla</title>');
-            newWin.document.write('<style>');
-            newWin.document.write('table { width: 100%; border-collapse: collapse; }');
-            newWin.document.write('th, td { border: 1px solid black; padding: 8px; text-align: left; }');
-            newWin.document.write('@media print { table tr td:last-child, table tr th:last-child { display: none; } }');
-            newWin.document.write('</style></head><body>');
-            newWin.document.write(divToPrint.outerHTML);
-            newWin.document.write('</body></html>');
-            newWin.document.close();
-            newWin.print();
-        } else {
-            alert("No se encontró la tabla para imprimir.");
-        }
-    }
-    </script>
+            function imprimirTabla() {
+                var divToPrint = document.getElementById("tablaDatos");
+                if (divToPrint) {
+                    var newWin = window.open("");
+                    newWin.document.write('<html><head><<title>Imprimir Tabla</title>');
+                    newWin.document.write('<style>');
+                    newWin.document.write('table { width: 100%; border-collapse: collapse; }');
+                    newWin.document.write('th, td { border: 1px solid black; padding: 8px; text-align: left; }');
+                    newWin.document.write('@media print { table tr td:last-child, table tr th:last-child { display: none; } }');
+                    newWin.document.write('</style></head><body>');
+                    newWin.document.write(divToPrint.outerHTML);
+                    newWin.document.write('</body></html>');
+                    newWin.document.close();
+                    newWin.print();
+                } else {
+                    alert("No se encontró la tabla para imprimir.");
+                }
+            }
+        </script>
+        <!-- Botones Agregar e Imprimir tabla -->
         <div class="button-container">
-        <button class="btn-primary" onclick="window.location.href='formulario.php'">Agregar</button>
-        <button class="btn-primary" onclick="imprimirTabla()">Imprimir Tabla</button>
-    </div>
+            <button class="btn-primary" onclick="window.location.href='formulario.php'">Agregar</button>
+            <button class="btn-primary" onclick="imprimirTabla()">Imprimir Tabla</button>
+        </div>
     </main>
     <br>
     <br>
@@ -112,4 +118,5 @@ $registros = mysqli_query($conexion, "select * from pacientes order by id DESC L
         <p>®️ 2024 Medical Stats. Todos los derechos reservados.</p>
     </footer>
 </body>
+
 </html>
